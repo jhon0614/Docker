92971a08da391c910e3976ad31b1857196ce339e
Jhon Ricardo Rios Cuervo <jhonri.0614@gmail.com>
2026-06-22 19:27:14 -0500

    Add event listener for button click in script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Landing cargada correctamente");
+
+    const button = document.querySelector("#btn");
+
+    if (button) {
+        button.addEventListener("click", () => {
+            alert("Hola desde GitHub Actions 🚀");
+        });
+    }
+});
